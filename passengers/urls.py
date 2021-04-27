d42d68fb4c42c2130d1aee2eb474eea9eb636103
Richard Farr <RichardFarr7@gmail.com>
2021-04-27 14:57:16 -0700

    quiz progress

--- passengers/urls.py
+++ passengers/urls.py
@@ -6,4 +6,5 @@ from . import views
 app_name = 'passengers'
 urlpatterns = [
     path('', TemplateView.as_view(template_name="home.html"), name='home'),
+    path('challenge', TemplateView.as_view(template_name="challenge.html"), name='challenge'),
 ]
